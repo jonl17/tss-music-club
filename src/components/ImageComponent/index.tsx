5584b3ffe33d7feed4b2aa3d82ec25842f1afdeb
jglorange <jglorange@gmail.com>
2021-01-30 11:03:47 +0000

    favicon

--- src/components/ImageComponent/index.tsx
+++ src/components/ImageComponent/index.tsx
@@ -1,23 +1,30 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect } from "react"
 import { GridItem } from "./styled"
 import Img from "gatsby-image"
-import { ImageComponentProps } from '~/types'
+import { ImageComponentProps } from "~/types"
 
-const Item: React.FC<ImageComponentProps> = ({ fluid, identifier, stopMotionContext, size }) => {
+const Item: React.FC<ImageComponentProps> = ({
+  fluid,
+  identifier,
+  stopMotionContext,
+  size,
+}) => {
   const { chosenImageId } = useContext(stopMotionContext)
   const [active, setActive] = useState(false)
 
   useEffect(() => {
     if (identifier === chosenImageId) {
       setActive(true)
-    }
-    else {
+    } else {
       setActive(false)
     }
   }, [chosenImageId, identifier])
 
   return (
-    <GridItem opacity={active ? "1" : "0"} padding={size === "large" ? "3rem" : "6rem"}>
+    <GridItem
+      opacity={active ? "1" : "0"}
+      padding={size === "large" ? "3rem" : "6rem"}
+    >
       <Img fluid={fluid}></Img>
     </GridItem>
   )
--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react"
 import { Helmet } from "react-helmet"
-import { PageName } from '~/types'
+import { PageName } from "~/types"
 
 type Props = {
-  pageName: PageName;
+  pageName: PageName
 }
 
 const Seo: React.FC<Props> = ({ pageName }) => {
   return (
     <Helmet>
       <title>{`TSS ${pageName ? "| " + pageName : ""}`}</title>
+      <link rel="icon" href="favi-tss.png" type="image/png"></link>
     </Helmet>
   )
 }
