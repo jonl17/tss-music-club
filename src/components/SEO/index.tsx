99eef393eb794d363d22115e141043458805d1df
jglorange <jglorange@gmail.com>
2021-03-13 21:43:50 +0000

    ffuuu

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -11,6 +11,24 @@ const Seo: React.FC<Props> = ({ pageName }) => {
     <Helmet>
       <title>{`TSS ${pageName ? "| " + pageName : ""}`}</title>
       <link rel="icon" href="favi-tss.png" type="image/png"></link>
+      <meta charSet="utf-8" />
+      <meta
+        name="description"
+        content="Musician, programmer and earth citizen from Iceland."
+      />
+      <meta name="keywords" content="Icelandic music, Icelandic programming" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta property="og:image" content="./gymnist/2.png" />
+      <meta
+        property="og:description"
+        content="Musician, programmer and earth citizen from Iceland."
+      />
+      <meta
+        name="twitter:description"
+        content="Musician, programmer and earth citizen from Iceland."
+      />
+      <meta name="twitter:image" content="./gymnist/2.png" />
+      <meta name="twitter:card" content="summary_large_image" />
     </Helmet>
   )
 }
